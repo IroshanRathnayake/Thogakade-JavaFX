20cb9551d71cee2cb8786dd3ebcc5544cd63d17b
IroshanRathnayake <meet.iroshan@gmail.com>
2024-08-15 19:10:38 +0530

    Implemented ViewCustomerForm & table features.

--- src/main/java/controller/ViewCustomerFormController.java
+++ src/main/java/controller/ViewCustomerFormController.java
@@ -1,4 +1,68 @@
 package controller;
 
-public class ViewCustomerFormController {
+import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customer;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ViewCustomerFormController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDOB;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableView<Customer> tblCustomers;
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+        loadTable();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadTable();
+    }
+
+    //Load data to table
+    private void loadTable(){
+        colID.setCellValueFactory(new PropertyValueFactory<>("customerID"));
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colDOB.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+
+        ObservableList<Customer> observableList = FXCollections.observableArrayList();
+        List<Customer> dbList = DBConnection.getInstance().getConnection();
+        observableList.addAll(dbList);
+
+        tblCustomers.setItems(observableList);
+    }
+
+
 }
